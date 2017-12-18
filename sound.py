5b9e26d10562f31322e02f42facddcd6f18f11f1
Karaz159 <karaz91@mail.ru>
2017-12-19 01:05:46 +0300

    now voice is bassboostable, and preparing other stuff

--- sound.py
+++ sound.py
@@ -3,13 +3,12 @@ import telebot as tb
 from datetime import datetime
 from os import system as sys
 from config import token as TOKEN
+from time import sleep
 
-def bass(how_many):
+def bass(how_many,inn,outt):
     apply_af = af().lowshelf(how_many)
-    infile = 'dwnld.wav'
-    outfile = 'bass.wav'
-    apply_af(infile, outfile)
-    print ('done')
+    apply_af(inn, outt)
+    print ('BASS done')
 
 def write_log(log):
     time = datetime.now()
@@ -31,6 +30,22 @@ def listener(messages):#достаточно полезная вещь, можн
         elif m.content_type == 'voice':
             write_log(str(m.chat.first_name) + ' ('+ str(m.chat.id) + ') '+ 'sended voice')
 
+def download_file(message, name):
+    try:
+        if message.content_type == 'audio':
+            file_info = bot.get_file(message.audio.file_id)
+        elif message.content_type == 'voice':
+            file_info = bot.get_file(message.voice.file_id)
+        downloaded_file = bot.download_file(file_info.file_path)
+        with open(name, 'wb') as new_file: #необходимо получить имя файла
+            new_file.write(downloaded_file)
+        return True
+    except tb.apihelper.ApiException:
+        return False
+
+def convert_to(inn, out):# необходимо понять как вставлять в sys переменные
+    sys('ffmpeg -y -loglevel quiet -i '+ inn + ' ' + out) #Конвертирую inn в ваф файл для дальнейшей работы
+
 bot = tb.TeleBot(TOKEN)
 bot.set_update_listener(listener)
 
@@ -39,22 +54,28 @@ def start(message):
     bot.send_message(message.chat.id, 'че пацаны, бассбуст?')
 
 @bot.message_handler(content_types=['audio'])
-def get_audio(message):
+def get_audio(message, nigga):
     bot.send_message(message.chat.id, 'эт аудио, инфа 100')
-    file_info = bot.get_file(message.audio.file_id)
-    downloaded_file = bot.download_file(file_info.file_path)
-    with open('new_file.mp3', 'wb') as new_file:
-        new_file.write(downloaded_file)
-    print('downloaded!')
-    sys('mpg123 -w dwnld.wav new_file.mp3') #Конвертирую new_file в ваф файл для дальнейшей работы
-    bass(25)# out is bass.wav
-    sys('lame -b 160 --vbr-new bass.wav send.mp3')
-    audio = open('send.mp3', 'rb')
-    bot.send_audio(message.chat.id, audio)
+    if download_file(message, 'new_file.mp3'):
+        print('downloaded!')
+        convert_to('new_file.mp3','dwnld.wav')
+        bass(50, 'dwnld.wav', 'bass.wav')# out is bass.wav
+        convert_to('bass.wav','send.mp3')
+        audio = open('send.mp3', 'rb')
+        bot.send_audio(message.chat.id, audio)
+    else:
+        bot.send_message(message.chat.id, 'Файлик слишком большой, прости, золотце')
 
 @bot.message_handler(content_types=['voice'])
 def get_voice(message):
     bot.send_message(message.chat.id, 'эт войс, я погромист, меня не обманешь')
-    bot.get_file('sound.ogg')
-
+    if download_file(message, 'new_voice.ogg'):
+        print('downloaded!')
+        convert_to('new_voice.ogg', 'new_voice.wav')
+        bass(100, 'new_voice.wav', 'new_voiceb.wav')
+        convert_to('new_voiceb.wav', 'send.ogg')
+    else:
+        bot.send_message(message.chat.id, 'СЛИШКОМ много болтовни, телеграм не позволяет скачать')
+    voice = open('send.ogg', 'rb')
+    bot.send_voice(message.chat.id, voice)
 bot.polling()
