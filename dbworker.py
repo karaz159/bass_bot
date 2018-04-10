a57fce8ac8fb240444b15022465f933b4f222a00
karaz159 <karaz91@mail.ru>
2018-04-11 00:37:41 +0300

    Added test feature + working on update reports

--- dbworker.py
+++ dbworker.py
@@ -1,19 +1,36 @@
 from vedis import Vedis
-import config
+import config, pickle
 import telebot as tb
-import config
 
 bot = tb.TeleBot(config.token)
 
+def conservate(user):
+    db = open('./stuff/users.dat', 'wb+')
+    users = pickle.load(db)
+    users.append(user)
+    pickle.dump(users, db)
+    db.close()
+
+def users_base():
+    try:
+        db = open('./stuff/users.dat', 'rb')
+        users = pickle.load(db)
+        db.close()
+        return users
+
+    except FileNotFoundError:
+        bot.send_message(config.karaz159, 'DATABASE NOT FOUND BOUYSSSSS', disable_web_page_preview=None, reply_to_message_id=None, reply_markup=None, parse_mode=None, disable_notification=None)
+        return config.karaz159
+    
 # Пытаемся узнать из базы «состояние» пользователя
-def get_current_state(user_id,message):
+def get_current_state(message):
     with Vedis(config.db_file) as db:
         try:
-            return db[user_id]
+            return db[message.chat.id]
         except KeyError:  # Если такого ключа почему-то не оказалось
+            conservate(message.chat.id)
             bot.forward_message(config.karaz159, message.chat.id, message.message_id)
             return config.States.S_START.value  # значение по умолчанию - начало диалога
-
 
 # Сохраняем текущее «состояние» пользователя в нашу базу
 def set_state(user_id, value):
