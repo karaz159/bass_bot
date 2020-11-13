2dbdfe11be7ec971a87ae1d873d78c9cb33e2cc5
Georgy <loh@kz159.ru>
2020-11-13 18:53:30 +0300

    Retry on tg connection exceptions (#4)

--- bass_bot/app.py
+++ bass_bot/app.py
@@ -2,6 +2,8 @@ from classes import BassBot
 from config import TOKEN
 from helpers import get_db_dsn
 from models import BASE
+from telebot import apihelper
+
+apihelper.RETRY_ON_ERROR = True
 
 bot = BassBot(TOKEN, get_db_dsn(), BASE)
-
